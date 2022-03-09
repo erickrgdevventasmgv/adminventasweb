03482f8da6f1c52f6dbef7bcf5888264f715a4a9
Erick Rivas Gomez <erickrgdev@gmail.com>
2022-03-09 00:22:13 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+//import { RouterLink } from "vue-router";
+import FirstView from "@/components/FirstView.vue";
+</script>
+
+<template>
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <FirstView></FirstView>
+
+      <nav></nav>
+    </div>
+  </header>
+</template>
---
+++ src/components/FirstView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="greetings">
+    <h1 class="green">Ventas Admin Web</h1>
+  </div>
+</template>
